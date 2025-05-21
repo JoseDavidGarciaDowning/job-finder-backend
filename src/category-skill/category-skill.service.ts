25588860dbc51b56a4eb14874a5b4d7f03958ab7
Jus141 <JUSTIN.PENA@ucr.ac.cr>
2025-05-20 18:52:54 -0600

    Se eliminan codigo comentado en category-skill.service

--- src/category-skill/category-skill.service.ts
+++ src/category-skill/category-skill.service.ts
@@ -1,41 +1,4 @@
-// import { Injectable } from '@nestjs/common';
-// import { db } from '../drizzle/db/db';
-// import { categorySkills } from '../drizzle/schema/schema';
-// import { CreateCategorySkillDto } from './dto/create-category-skill.dto';
-// import { UpdateCategorySkillDto } from './dto/update-category-skill.dto';
-// import { eq } from 'drizzle-orm';
 
-// @Injectable()
-// export class CategorySkillService {
-//   create(createCategorySkillDto: CreateCategorySkillDto) {
-   
-//     return db.insert(categorySkills).values(createCategorySkillDto).returning();
-//   }
-
-//   findAll() {
-    
-//       return db.select().from(categorySkills);
-
-//   }
-
-//   findOne(id: string) {
-  
-//        return db.select().from(categorySkills).where(eq(categorySkills.id, id));
-
-//   }
-
-//   update(id: string, updateCategorySkillDto: UpdateCategorySkillDto) {
-  
-//     return db.update(categorySkills).set(updateCategorySkillDto).where(eq(categorySkills.id, id)).returning();
-
-//   }
-
-//   remove(id: string) {
-   
-//     return db.delete(categorySkills).where(eq(categorySkills.id, id)).returning();
-
-//   }
-// }
 import { Injectable } from '@nestjs/common';
 import { db } from '../drizzle/db/db';
 import { categorySkills, categories, skills } from '../drizzle/schema/schema';
